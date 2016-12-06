a8b614b8a9fc1dc7d4d0d06c1e29ff05f3f90327
Michael Peng <michael@mcmoo.org>
2016-12-05 21:16:58 -0800

    Day6

---
+++ day6p2.rb
@@ -0,0 +1,18 @@
+# Day 6 of Advent Of Code, 2016
+
+INPUT = File.read("data/6.txt")
+
+occurrence_position = Array.new(8, '')
+
+INPUT.each_line do |line|
+  8.times do |i|
+    occurrence_position[i] += line[i]
+  end
+end
+
+output = ''
+8.times do |i|
+  output += occurrence_position[i].split('').group_by(&:itself).values.min_by(&:size).first
+end
+
+puts "the message is #{output}"
