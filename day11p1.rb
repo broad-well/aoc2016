d34fe9cb2effc095af475b6c3ef710b5446cbcb5
broad-well <michaelpeng2002@icloud.com>
2016-12-13 21:47:23 -0800

    Day14

--- day11p1.rb
+++ day11p1.rb
@@ -5,7 +5,7 @@ require 'fiber'
 
 $floors = [['pr-g', 'pr-m'], ['co-g', 'cu-g', 'ru-g', 'pl-g'],
            ['co-m', 'cu-m', 'ru-m', 'pl-m'], []]
-$floors = [['hy-m', 'li-m'], ['hy-g'], ['li-g'], []] if ARGV.include? 'use-example'
+#$floors = [['hy-m', 'li-m'], ['hy-g'], ['li-g'], []] if ARGV.include? 'use-example'
 $elevator = 0
 
 def pair_element(elem)
@@ -91,7 +91,7 @@ def reverse_act(action)
 end
 
 def done(floors)
-  floors[0..2].join.length == 0
+  floors[0..2].join.empty?
 end
 
 if ARGV.length == 0
@@ -136,17 +136,60 @@ def internal_max(elev, floors, prev_act, level = 0)
   level.zero? ? step_scores.key(step_scores.values.max) : step_scores.values.max
 end
 
+class StateHash < Hash
+  def initialize
+    super
+  end
+
+  def [](state)
+    method(:[]).super_method.call(state_tostr(state))
+  end
+
+  def []=(state, val)
+    method(:[]=).super_method.call(state_tostr(state), val.to_i)
+  end
+
+  def include?(state)
+    method(:include?).super_method.call(state_tostr(state))
+  end
+
+  def get(elev, floors)
+    self[[elev, floors]]
+  end
+
+  private
+
+  def state_tostr(state)
+    floorstr = ''
+    state[1].each do |floor|
+      floorstr += floor.join('') + ','
+    end
+    state[0].to_s + floorstr[0..-2]
+  end
+end
+
+def bfs # theoretical confidence
+  queue = [[$elevator, $floors.map(&:dup).dup]]
+  dist_prev = StateHash.new
+  dist_prev[queue[0]] = 0
+
+  loop do
+    current_e, current_f = queue.pop
+    current_d = dist_prev.get(current_e, current_f)
+    return current_d if done current_f
+    permutations_cond(current_f, current_e).each do |action|
+      new_state = virtual_act(current_e, current_f.map(&:dup).dup, action)
+      next if dist_prev.include?(new_state)
+      dist_prev[new_state] = current_d + 1
+      queue.insert(0, new_state)
+      #print "queue.count=#{queue.count} dist_prev.count=#{dist_prev.count}"\
+      #      " current_d=#{current_d}\r"
+    end
+  end
+end
+
 def max
   internal_max($elevator, $floors, 'u-NA')
 end
 
-stepcount = 0
-until $floors[0..2].map(&:empty?).all?
-  step = max
-  p step
-  act step
-  stepcount += 1
-  puts "floors now: #{$floors}"
-end
-
-puts "In #{stepcount} steps"
+puts bfs
---
+++ day14p2.py
@@ -0,0 +1,44 @@
+# Day 15 of Advent of Code
+
+import hashlib
+salt = 'zpqevtbw'
+#salt = 'abc'
+
+padkeys = []
+
+def repeat(charlen, _str):
+    o = 0
+    while o <= len(_str) - charlen:
+        if len(set(_str[o:o+charlen])) == 1: return _str[o]
+        o += 1
+    return None
+
+i = 0
+# repeat_criteria: hash index in which the 1000 hashes end, character, pending padkey
+repeat_criteria = []
+
+while len(padkeys) < 64:
+    digest = hashlib.md5("{}{}".format(salt, i).encode()).hexdigest()
+    for p in range(2016):
+        digest = hashlib.md5(digest.encode()).hexdigest()
+    rep5 = repeat(5, digest)
+    rep3 = repeat(3, digest)
+    rc_del = []
+
+    for rc in repeat_criteria:
+        if rc[1] == rep5 and rc[0] >= i:
+            padkeys.append((rc[2], rc[0] - 1000))
+            rc_del.append(rc)
+        if rc[0] < i:
+            rc_del.append(rc)
+    for rd in rc_del:
+        repeat_criteria.remove(rd)
+
+    if rep3 != None:
+        repeat_criteria.append((i + 1000, rep3, digest))
+
+    if i % 100 == 0:
+        print(i, end="\r") 
+    i += 1
+
+print("Answer: {}".format(padkeys[63]))
