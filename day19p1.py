e827b0e0ea2014ab3eeab5c17e79301b58ba2ed1
Michael Peng <michael@mcmoo.org>
2016-12-18 23:45:26 -0800

    Correct Day19p1

--- day19p1.py
+++ day19p1.py
@@ -1,15 +1,6 @@
 # Day 19 of Advent of Code
-from math import floor
 
-elves = list(range(1, 3012211))
-
-while len(elves) > 1:
-    print(len(elves))
-    indexes = []
-    indexes += list(range(1, floor(len(elves)/2+1)))
-    if len(elves) % 2 == 1:
-        indexes.append(0)
-    for i in indexes:
-        elves.pop(i)
-
-print(elves[0])
+i = 0
+for o in range(1, 3012211):
+    i = (i+2)%o
+print(i+1)
